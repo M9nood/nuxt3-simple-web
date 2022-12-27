0a51f8ef67a6c966c551ea631a9b56912b0a63aa
toeiminal <toei@finnomena.com>
2022-12-28 00:05:12 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Indecsdcdx Page</h1>
+    <p>counter : {{ counter.count }}</p>
+    <p>
+      <button @click="counter.increment">add</button>
+    </p>
+  </div>
+</template>
+
+<script setup>
+const counter = useCounter()
+useHead({
+  title: 'Nuxt3 - Demo App'
+})
+console.log('Hello world!', counter.count)
+</script>
